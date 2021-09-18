85a2be651dfdd5d008d08014b0ade8ebfee56cd0
Illia Aprol <asdf21072107@gmail.com>
2021-09-18 21:14:36 +0300

    Add response struct and json convert

--- server.go
+++ server.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"time"
 )
 
+type response struct {
+	Time string `json:"time"`
+}
+
 func main() {
 
+	result, _ := json.Marshal(response{time.Now().Format(time.RFC3339)})
+
 	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, time.Now().Format(time.RFC3339))
+		fmt.Fprintf(w, string(result))
 	})
 
 	fmt.Printf("Starting server at port 8795\n")
