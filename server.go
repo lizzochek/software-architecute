9c632d97c7f8a41b4a26f863629db36bbbfc97b6
Illia Aprol <asdf21072107@gmail.com>
2021-09-18 20:57:24 +0300

    Add cuurent time display

--- server.go
+++ server.go
@@ -1,19 +1,20 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
 )
 
 func main() {
 
-	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request){
-        fmt.Fprintf(w, "time!")
-    })
+	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, time.Now().String())
+	})
 
-    fmt.Printf("Starting server at port 8795\n")
-    if err := http.ListenAndServe(":8795", nil); err != nil {
-        log.Fatal(err)
-    }
+	fmt.Printf("Starting server at port 8795\n")
+	if err := http.ListenAndServe(":8795", nil); err != nil {
+		log.Fatal(err)
+	}
 }
